b3c2af3e0bca5e12b825d2a28bf0bf6852ae2975
Vitaly Baev <ping@baev.dev>
2022-07-13 20:17:39 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,12 @@ const MainPage: FC = () => {
       <h1 className="font-sans font-extrabold text-4xl">Виталий Баев</h1>
       <p className="mt-2">Front-end разработчик</p>
       <p className="mt-2">JavaScript, TypeScript, React, Redux</p>
-      <!--
+      {/*
         <p className="mt-2">
           <span className="opacity-50">🇷🇺 Москва &rarr; 🇹🇭 Бангкок &rarr; </span>
           🇧🇾 Минск
         </p>
-      -->
+      */}
       <div className="mt-6">
         <a href="mailto:ping@baev.dev">ping@baev.dev</a>
       </div>
